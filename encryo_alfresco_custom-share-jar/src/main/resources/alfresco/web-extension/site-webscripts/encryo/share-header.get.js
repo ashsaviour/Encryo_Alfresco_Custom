58b58118c0400863317f80d830bae72440d5382a
ashsaviour <ashsaviour@sina.com>
2020-01-07 16:02:00 +0800

    first publish

--- encryo_alfresco_custom-share-jar/src/main/resources/alfresco/web-extension/site-webscripts/encryo/share-header.get.js
+++ encryo_alfresco_custom-share-jar/src/main/resources/alfresco/web-extension/site-webscripts/encryo/share-header.get.js
@@ -1,4 +1,4 @@
-widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_MY_FILES");
+// widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_MY_FILES");
 widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_TASKS");
 if (!user.isAdmin)
 {
@@ -7,13 +7,13 @@ if (!user.isAdmin)
 var HomeMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_HOME");
 if (HomeMenu != null)
 {
-    HomeMenu.config.targetUrl = "http://cnmecwss01v/sites/maison/exquiso/index_alfresco.html";
+    HomeMenu.config.targetUrl = "https://cnmecwss03v/index.jsp";
     HomeMenu.config.targetUrlType = "FULL_PATH";
 }
-var SitesMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
-if (SitesMenu!=null)
-{
-    SitesMenu.config.label = "Projects";
-    SitesMenu.config.description = "Projects desc";
-}
+// var SitesMenu = widgetUtils.findObject(model.jsonModel, "id", "HEADER_SITES_MENU");
+// if (SitesMenu!=null)
+// {
+//     SitesMenu.config.label = "Projects";
+//     SitesMenu.config.description = "Projects desc";
+// }
 widgetUtils.deleteObjectFromArray(model.jsonModel, "id", "HEADER_USER_MENU_HOME_PAGE_GROUP");
